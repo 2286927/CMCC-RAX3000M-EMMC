d083fa551b1cf7c0516acd28e17f007665bfed59
ChenChen <cc2286927@sina.cn>
2025-10-06 22:29:06 +0800

    Update diy-part1.sh

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -10,7 +10,7 @@
 #echo 'src-git small https://github.com/kenzok8/small.git;master' >>feeds.conf.default
 #灵缇游戏加速器(feeds.conf.default中istore-packages.git已经包含luci-app-LingTiGameAcc,但不包含依赖项LingTiGameAcc)
 #git clone https://github.com/esirplayground/luci-app-LingTiGameAcc.git feeds/third_party/luci-app-LingTiGameAcc
-git clone https://github.com/esirplayground/LingTiGameAcc.git feeds/third_party/LingTiGameAcc
+#git clone https://github.com/esirplayground/LingTiGameAcc.git feeds/third_party/LingTiGameAcc
 
 #==============删除重复feed源===================
 #rm -rf feeds/third_party/luci-app-LingTiGameAcc
